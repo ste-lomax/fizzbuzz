78b6fedc21f1e9d5476a2e84f4d81adcda104ae7
Steven Lomax <ste.lomax@gmail.com>
2022-01-15 16:36:18 +0000

    Working FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,11 @@
 for (let number =1; number <= 100; number = number + 1 ) {
     if ((number % 3 === 0) && (number % 5 === 0)) {
         console.log('FizzBuzz');
-    } if else (number % 3 === 0) {
+    } else if (number % 3 === 0) {
         console.log('Fizz');
-    } if else (number % 5 === 0) {
+    } else if (number % 5 === 0) {
         console.log('Buzz');
     } else {
-        console.log('number');
+        console.log(number);
     }
 };
