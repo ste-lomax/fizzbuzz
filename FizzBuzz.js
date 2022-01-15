dd106e6d98fd23b8964d94da6527a9196098b915
Jordan Robinson <me@jordanrobinson.co.uk>
2022-01-15 16:43:05 +0000

    Adds an edge case

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,12 @@
-for (let number =1; number <= 100; number = number + 1 ) {
+for (let number = 1; number <= 100; number = number + 1) {
     if ((number % 3 === 0) && (number % 5 === 0)) {
         console.log('FizzBuzz');
     } else if (number % 3 === 0) {
         console.log('Fizz');
     } else if (number % 5 === 0) {
         console.log('Buzz');
+    } else if (number === 100) {
+        console.log('Hi Ste')
     } else {
         console.log(number);
     }
